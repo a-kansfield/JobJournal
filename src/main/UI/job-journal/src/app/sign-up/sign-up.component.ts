32c6339f4234b622423697fe1bfd5573f0eed3df
Amber <akansf1@gmail.com>
2025-05-14 04:44:55 -0500

    Sign up now alerts users if their email address is already in use.

--- src/main/UI/job-journal/src/app/sign-up/sign-up.component.ts
+++ src/main/UI/job-journal/src/app/sign-up/sign-up.component.ts
@@ -5,17 +5,19 @@ import { ActivatedRoute, Router } from '@angular/router';
 import { UserDataService } from '../data/service/user/user-data.service';
 import { HttpHeaders } from '@angular/common/http';
 import { AuthService } from '../service/authentication/auth.service';
+import { NgIf } from '@angular/common';
 
 @Component({
   selector: 'app-sign-up',
-  imports: [FormsModule],
+  imports: [FormsModule, NgIf],
   templateUrl: './sign-up.component.html',
   styleUrl: './sign-up.component.css'
 })
 
 export class SignUpComponent {
   user! : User;
-
+  err : boolean = false;
+  errorMsg : string = 'Email already in use.'
   constructor(
     private userService : UserDataService,
     private route : ActivatedRoute,
@@ -34,10 +36,14 @@ export class SignUpComponent {
         console.log("User saved");
         this.authService.executeAuthService(this.user.email, this.user.password).subscribe(
           data => {
+            this.err = false;
             this.router.navigate(['applications']);
           }
         )
 
+      },
+      error => {
+        this.err = true;
       }
     )
   }
